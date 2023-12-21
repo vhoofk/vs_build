9fdf5c59d415ee4fd25a4ae73d3f66c93252c7d8
vhoofk <vhoofk@gmail.com>
2023-12-21 13:34:07 +0100

    Add license to build script header

--- vs_build.bat
+++ vs_build.bat
@@ -1,5 +1,32 @@
 @echo off
 
+rem --------------------------------------------------------------------------
+rem BSD 2-Clause License
+rem 
+rem Copyright (c) 2023, Kris Vanhoof
+rem 
+rem Redistribution and use in source and binary forms, with or without
+rem modification, are permitted provided that the following conditions are met:
+rem 
+rem 1. Redistributions of source code must retain the above copyright notice, this
+rem    list of conditions and the following disclaimer.
+rem 
+rem 2. Redistributions in binary form must reproduce the above copyright notice,
+rem    this list of conditions and the following disclaimer in the documentation
+rem    and/or other materials provided with the distribution.
+rem 
+rem THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+rem AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+rem IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+rem DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+rem FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+rem DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+rem SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+rem CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+rem OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+rem OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+rem --------------------------------------------------------------------------
+
 rem
 rem Build using Visual Studio development environment:
 rem
